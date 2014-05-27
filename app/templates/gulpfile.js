26bc66a1cce85833509a4623c29f6f765a409f25
niallobrien <me@niallobrien.me>
2014-05-27 03:32:11 +0100

    Fixes scripts & build tasks.

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -22,7 +22,7 @@ gulp.task('styles', function () {
 gulp.task('scripts', function () {
     return gulp.src('app/scripts/**/*.js')
         .pipe($.jshint())
-        .pipe($.jshint.reporter(require(jshint-stylish)))
+        .pipe($.jshint.reporter(require('jshint-stylish')))
         .pipe($.size());
 });
 
